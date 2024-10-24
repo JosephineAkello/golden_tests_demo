815ac677d4d0b908459d6bb0596795f582e00265
JosephineAkello <josephineakello2030@gmail.com>
2024-10-24 14:58:20 +0200

    update home and remove initial tests

--- lib/main.dart
+++ lib/main.dart
@@ -15,30 +15,39 @@ class MyApp extends StatelessWidget {
               seedColor: const Color.fromARGB(255, 14, 11, 207)),
           useMaterial3: true,
         ),
-        home: Scaffold(
-          appBar: AppBar(
-            title: const Text('Golden Tests Button Colors Demo'),
-          ),
-          body: Center(
-            child: ElevatedButton(
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.purple,
-                padding: const EdgeInsets.symmetric(
-                  horizontal: 50,
-                  vertical: 20,
-                ),
-                textStyle: const TextStyle(
-                  fontSize: 20,
-                  fontWeight: FontWeight.bold,
-                ),
-              ),
-              onPressed: () {},
-              child: const Text(
-                'Flutter Goldens',
-                style: TextStyle(color: Colors.white),
-              ),
+        home: const HomePageButton());
+  }
+}
+
+class HomePageButton extends StatelessWidget {
+  const HomePageButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Golden Tests Button Colors Demo'),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: Colors.purple,
+            padding: const EdgeInsets.symmetric(
+              horizontal: 50,
+              vertical: 20,
+            ),
+            textStyle: const TextStyle(
+              fontSize: 20,
+              fontWeight: FontWeight.bold,
             ),
           ),
-        ));
+          onPressed: () {},
+          child: const Text(
+            'Flutter Goldens',
+            style: TextStyle(color: Colors.white),
+          ),
+        ),
+      ),
+    );
   }
 }
